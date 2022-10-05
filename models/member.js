eb7f8ccce67f08783b16720c9c1d5f4521c5baf6
abdulmalekFahd <abdulamlk819@gmail.com>
2022-10-05 14:27:27 +0300

    setup moduls

--- models/member.js
+++ models/member.js
@@ -17,7 +17,8 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     phone: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    gender: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Member',
